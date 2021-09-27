f42a9648c87034cc896a5ec9c44766fd67ebed55
AdahMilly <oamildred@gmail.com>
2021-09-27 13:47:47 -0400

    add navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import React from "react"
-import SideBar from "./components/SideBar/SideBar";
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-function App() {
+import NavBar from './components/NavBar/NavBar';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import Projects from './components/pages/Projects';
+import SideBar from './components/SideBar/SideBar';
+
+const App = () => {
   return (
-    <div>
-      <SideBar />
-    </div>
+    <>
+      <BrowserRouter>
+        {<NavBar />}
+        <Route path="/signin" component={SignIn} />
+        <Route path="/signup" component={SignUp} />
+        <Route path="/" exact component={Projects} />
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { AppBar, Typography, Toolbar, Button } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  linkStyle: {
+    color: '#fafafa',
+    textDecoration:'none'
+  },
+});
+
 const NavBar = () => {
+
+    const classes = useStyles()
     return (
         <>
-            <p>This the navbar</p>
+            <AppBar position="static">
+                <Typography variant="h4">
+                    <Link className={ classes.linkStyle} to="/">
+                        projectManagement
+                    </Link>
+                </Typography>
+            </AppBar>
         </>
      );
 }
