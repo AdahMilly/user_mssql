0774d9cae187c79769a62d9289d2f3b2c85dc4e1
AdahMilly <oamildred@gmail.com>
2021-10-25 16:41:52 +0300

    add border color

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -8,7 +8,7 @@ import { makeStyles } from "@mui/styles";
 
 const useStyles = makeStyles({
   linkStyle: {
-    color: "#00000",
+    borderColor:"blue",
     textDecoration: "none",
   },
 });
@@ -17,7 +17,7 @@ const SideBar = () => {
   const classes = useStyles();
   return (
     <div className="sidebar">
-        <NavLink className={classes.linkStyle} to="/dashboard" style={{width:"100px", alignSelf:"center",border:"solid",margin:"20px",padding:"15px",fontSize:"1.2rem"}}>
+        <NavLink className={classes.linkStyle} to="/dashboard" style={{width:"100px", alignSelf:"center",border:"solid ##0b5bdb",margin:"20px",padding:"10px",fontSize:"1.2rem",color:"black"}}>
           Dashboard
         </NavLink>
       <Button className="acc-btn" variant={"outlined"} style={{width:"100px", alignSelf:"center"}}>
